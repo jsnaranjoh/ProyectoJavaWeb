2167f829dcdfcf21cc67d03d7e6e727c7bdc2ccd
Christian1994 <crisdanovagoodyear@hotmail.com>
2016-05-08 21:02:10 -0500

    Bosquejo de Lógica para aptitudes de Ingeniero de Software creado.

---
+++ src/java/logica/IngsoftwaresistemaoperativoLogica.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author NOREÑA
+ */
+@Stateless
+public class IngsoftwaresistemaoperativoLogica implements IngsoftwaresistemaoperativoLogicaLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
