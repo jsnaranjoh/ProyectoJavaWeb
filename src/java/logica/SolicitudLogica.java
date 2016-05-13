9b5a39baa0d226256ba0f6a4aec5b0d5bb5e01b3
Christian1994 <crisdanovagoodyear@hotmail.com>
2016-05-13 13:34:53 -0500

    Lógica Solicitud creada. Capa Lógica del Proyecto terminada.
    Revisar por si hay que cambiar o agregar detalles a esa capa.

--- src/java/logica/SolicitudLogica.java
+++ src/java/logica/SolicitudLogica.java
@@ -6,8 +6,10 @@
 package logica;
 
 import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import modelo.Solicitud;
+import persistencia.SolicitudFacadeLocal;
 
 /**
  *
@@ -16,24 +18,126 @@ import modelo.Solicitud;
 @Stateless
 public class SolicitudLogica implements SolicitudLogicaLocal {
 
+    @EJB
+    SolicitudFacadeLocal solicitudDAO;
+    
     @Override
     public void registrarSolicitud(Solicitud solicitud) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(solicitud.getRequisito().getCodigo() == null){
+            throw new Exception("Debes seleccionar el Requisito al cual se le realiza la Solicitud.");
+        }
+        if(solicitud.getVerificador().getCedula() == null){
+            throw new Exception("Debes seleccionar el Jefe que verifica la Solicitud.");
+        }
+        if(solicitud.getSolicitante().getCedula() == null){
+            throw new Exception("Debes seleccionar el Ingeniero de Software que realiza la Solicitud.");
+        }
+        if(solicitud.getTipo().equals("")){
+            throw new Exception("Campo Tipo Solicitud Obligatorio.");
+        }
+        if(solicitud.getTitulo().equals("")){
+            throw new Exception("Campo Título Solicitud Obligatorio.");
+        }
+        if(solicitud.getFecha() == null){
+            throw new Exception("Campo Fecha Solicitud Obligatorio.");
+        }
+        // Campo Origen sí puede ir vacío
+        if(solicitud.getEstado().equals("")){
+            throw new Exception("Campo Estado Solicitud Obligatorio.");
+        }
+        if(solicitud.getFechaultimaactualizacion() == null){
+            throw new Exception("Campo Fecha de Última Actualización de Solicitud Obligatorio.");
+        }
+        if(solicitud.getRelease().equals("")){
+            throw new Exception("Campo Release de Solicitud Obligatorio.");
+        }
+        if(solicitud.getDescripcion().equals("")){
+            throw new Exception("Campos Descripción de Solicitud Obligatorio.");
+        }
+        // Campo Comentarios sí puede ir vacío
+        
+        Solicitud objSolicitud = solicitudDAO.find(solicitud.getNumero());
+        if(objSolicitud != null){
+            throw new Exception("Solicitud ya existe.");
+        }
+        else{
+            solicitudDAO.create(solicitud);
+        }
     }
 
     @Override
     public void modificarSolicitud(Solicitud solicitud) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(solicitud.getRequisito().getCodigo() == null){
+            throw new Exception("Debes seleccionar el Requisito al cual se le realiza la Solicitud.");
+        }
+        if(solicitud.getVerificador().getCedula() == null){
+            throw new Exception("Debes seleccionar el Jefe que verifica la Solicitud.");
+        }
+        if(solicitud.getSolicitante().getCedula() == null){
+            throw new Exception("Debes seleccionar el Ingeniero de Software que realiza la Solicitud.");
+        }
+        if(solicitud.getTipo().equals("")){
+            throw new Exception("Campo Tipo Solicitud Obligatorio.");
+        }
+        if(solicitud.getTitulo().equals("")){
+            throw new Exception("Campo Título Solicitud Obligatorio.");
+        }
+        if(solicitud.getFecha() == null){
+            throw new Exception("Campo Fecha Solicitud Obligatorio.");
+        }
+        // Campo Origen sí puede ir vacío
+        if(solicitud.getEstado().equals("")){
+            throw new Exception("Campo Estado Solicitud Obligatorio.");
+        }
+        if(solicitud.getFechaultimaactualizacion() == null){
+            throw new Exception("Campo Fecha de Última Actualización de Solicitud Obligatorio.");
+        }
+        if(solicitud.getRelease().equals("")){
+            throw new Exception("Campo Release de Solicitud Obligatorio.");
+        }
+        if(solicitud.getDescripcion().equals("")){
+            throw new Exception("Campos Descripción de Solicitud Obligatorio.");
+        }
+        // Campo Comentarios sí puede ir vacío
+        
+        Solicitud objSolicitud = solicitudDAO.find(solicitud.getNumero());
+        if(objSolicitud == null){
+            throw new Exception("Solicitud a modificar no existe.");
+        }
+        else{
+            objSolicitud.setRequisito(solicitud.getRequisito());
+            objSolicitud.setVerificador(solicitud.getVerificador());
+            objSolicitud.setSolicitante(solicitud.getSolicitante());
+            objSolicitud.setTipo(solicitud.getTipo());
+            objSolicitud.setTitulo(solicitud.getTitulo());
+            objSolicitud.setFecha(solicitud.getFecha());
+            objSolicitud.setOrigen(solicitud.getOrigen());
+            objSolicitud.setEstado(solicitud.getEstado());
+            objSolicitud.setPrioridadsolicitante(solicitud.getPrioridadsolicitante());
+            objSolicitud.setPrioridadrealizacion(solicitud.getPrioridadrealizacion());
+            objSolicitud.setFechaultimaactualizacion(solicitud.getFechaultimaactualizacion());
+            objSolicitud.setRelease(solicitud.getRelease());
+            objSolicitud.setEsfuerzo(solicitud.getEsfuerzo());
+            objSolicitud.setDescripcion(solicitud.getDescripcion());
+            objSolicitud.setComentarios(solicitud.getComentarios());
+            solicitudDAO.edit(solicitud);
+        }
     }
 
     @Override
     public void eliminarSolicitud(Solicitud solicitud) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Solicitud objSolicitud = solicitudDAO.find(solicitud.getNumero());
+        if(objSolicitud == null){
+            throw new Exception("Solicitud a eliminar no existe.");
+        }
+        else{
+            solicitudDAO.remove(solicitud);
+        }
     }
 
     @Override
     public List<Solicitud> consultarTodas() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return solicitudDAO.findAll();
     }
 
     // Add business logic below. (Right-click in editor and choose
