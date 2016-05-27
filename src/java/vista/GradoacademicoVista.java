7abe94e58903d9041ed1137bab3ae189068aa05d
Christian1994 <crisdanovagoodyear@hotmail.com>
2016-05-26 21:19:21 -0500

    Capa Vista y Cliente Grado Académico Ing. Software creadas.
    Aún queda pendiente el CRUD.

---
+++ src/java/vista/GradoacademicoVista.java
@@ -0,0 +1,225 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.model.SelectItem;
+import logica.GradoacademicoLogicaLocal;
+import logica.IngsoftwareLogicaLocal;
+import modelo.Gradoacademico;
+import modelo.Ingsoftware;
+import org.primefaces.component.commandbutton.CommandButton;
+import org.primefaces.component.inputtext.InputText;
+import org.primefaces.component.selectonemenu.SelectOneMenu;
+import org.primefaces.event.SelectEvent;
+
+/**
+ *
+ * @author NOREÑA
+ */
+@Named(value = "gradoacademicoVista")
+@RequestScoped
+public class GradoacademicoVista {
+
+    private InputText txtNumero;
+    private SelectOneMenu cmbIngenieros;
+    private ArrayList<SelectItem> itemsIngenieros;
+    private SelectOneMenu cmbNiveles;
+    private InputText txtLugar;
+    private InputText txtAnioTitulacion;
+    private InputText txtTituloObtenido;
+
+    private List<Gradoacademico> listaGradosacademicos;
+    private Gradoacademico selectedGradoacademico;
+    
+    private CommandButton btnRegistrar;
+    private CommandButton btnModificar;
+    private CommandButton btnEliminar;
+    private CommandButton btnLimpiar;
+
+    @EJB
+    GradoacademicoLogicaLocal gradoacademicoLogica;
+    
+    @EJB
+    IngsoftwareLogicaLocal ingsoftwareLogica;
+    
+    public InputText getTxtNumero() {
+        return txtNumero;
+    }
+
+    public void setTxtNumero(InputText txtNumero) {
+        this.txtNumero = txtNumero;
+    }
+
+    public SelectOneMenu getCmbIngenieros() {
+        return cmbIngenieros;
+    }
+
+    public void setCmbIngenieros(SelectOneMenu cmbIngenieros) {
+        this.cmbIngenieros = cmbIngenieros;
+    }
+
+    public ArrayList<SelectItem> getItemsIngenieros() {
+        try {
+            List<Ingsoftware> listaIng = ingsoftwareLogica.consultarTodos();
+            itemsIngenieros = new ArrayList<>();
+            
+            for(Ingsoftware ing: listaIng){
+                itemsIngenieros.add(new SelectItem(ing.getCedula(), ing.getNombres() + " " + ing.getApellidos()));
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(JefeVista.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return itemsIngenieros;
+    }
+
+    public void setItemsIngenieros(ArrayList<SelectItem> itemsIngenieros) {
+        this.itemsIngenieros = itemsIngenieros;
+    }
+
+    public SelectOneMenu getCmbNiveles() {
+        return cmbNiveles;
+    }
+
+    public void setCmbNiveles(SelectOneMenu cmbNiveles) {
+        this.cmbNiveles = cmbNiveles;
+    }
+
+    public InputText getTxtLugar() {
+        return txtLugar;
+    }
+
+    public void setTxtLugar(InputText txtLugar) {
+        this.txtLugar = txtLugar;
+    }
+
+    public InputText getTxtAnioTitulacion() {
+        return txtAnioTitulacion;
+    }
+
+    public void setTxtAnioTitulacion(InputText txtAnioTitulacion) {
+        this.txtAnioTitulacion = txtAnioTitulacion;
+    }
+
+    public InputText getTxtTituloObtenido() {
+        return txtTituloObtenido;
+    }
+
+    public void setTxtTituloObtenido(InputText txtTituloObtenido) {
+        this.txtTituloObtenido = txtTituloObtenido;
+    }
+
+    public List<Gradoacademico> getListaGradosacademicos() {
+        if(listaGradosacademicos == null){
+            try {
+                listaGradosacademicos = gradoacademicoLogica.consultarTodos();
+            } catch (Exception ex) {
+                Logger.getLogger(GradoacademicoVista.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        return listaGradosacademicos;
+    }
+
+    public void setListaGradosacademicos(List<Gradoacademico> listaGradosacademicos) {
+        this.listaGradosacademicos = listaGradosacademicos;
+    }
+
+    public Gradoacademico getSelectedGradoacademico() {
+        return selectedGradoacademico;
+    }
+
+    public void setSelectedGradoacademico(Gradoacademico selectedGradoacademico) {
+        this.selectedGradoacademico = selectedGradoacademico;
+    }
+
+    public CommandButton getBtnRegistrar() {
+        return btnRegistrar;
+    }
+
+    public void setBtnRegistrar(CommandButton btnRegistrar) {
+        this.btnRegistrar = btnRegistrar;
+    }
+
+    public CommandButton getBtnModificar() {
+        return btnModificar;
+    }
+
+    public void setBtnModificar(CommandButton btnModificar) {
+        this.btnModificar = btnModificar;
+    }
+
+    public CommandButton getBtnEliminar() {
+        return btnEliminar;
+    }
+
+    public void setBtnEliminar(CommandButton btnEliminar) {
+        this.btnEliminar = btnEliminar;
+    }
+
+    public CommandButton getBtnLimpiar() {
+        return btnLimpiar;
+    }
+
+    public void setBtnLimpiar(CommandButton btnLimpiar) {
+        this.btnLimpiar = btnLimpiar;
+    }
+    
+    public void onRowSelect(SelectEvent event){
+        this.selectedGradoacademico = (Gradoacademico) event.getObject();
+        this.txtNumero.setValue(this.selectedGradoacademico.getNumero());
+        this.cmbIngenieros.setValue(this.selectedGradoacademico.getIngeniero().getCedula());
+        this.cmbNiveles.setValue(this.selectedGradoacademico.getNivel());
+        this.txtLugar.setValue(this.selectedGradoacademico.getLugar());
+        this.txtAnioTitulacion.setValue(this.selectedGradoacademico.getAniotitulacion());
+        this.txtTituloObtenido.setValue(this.selectedGradoacademico.getTituloobtenido());
+        
+        this.btnRegistrar.setDisabled(true);
+        this.btnModificar.setDisabled(false);
+        this.btnEliminar.setDisabled(false);
+        this.txtNumero.setDisabled(true);        
+    }
+    
+    public void limpiar(){
+        this.txtNumero.setValue("");
+        this.cmbIngenieros.setValue("");
+        this.cmbNiveles.setValue("");
+        this.txtLugar.setValue("");
+        this.txtAnioTitulacion.setValue("");
+        this.txtTituloObtenido.setValue("");
+        
+        this.txtNumero.setDisabled(false);
+        this.btnRegistrar.setDisabled(false);
+        this.btnModificar.setDisabled(true);
+        this.btnEliminar.setDisabled(true);         
+    }
+    
+    public void action_registrar(){
+        
+    }
+    
+    public void action_modificar(){
+        
+    }
+
+    public void action_eliminar(){
+        
+    }    
+    
+    /**
+     * Creates a new instance of GradoacademicoVista
+     */
+    public GradoacademicoVista() {
+    }
+    
+}
---
+++ src/java/vista/SeminarioVista.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import org.primefaces.event.SelectEvent;
+
+/**
+ *
+ * @author NOREÑA
+ */
+@Named(value = "seminarioVista")
+@RequestScoped
+public class SeminarioVista {
+
+    public void onRowSelect(SelectEvent event){
+        
+    }
+
+    public void limpiar(){
+        
+    }
+    
+    public void action_registrar(){
+        
+    }
+    
+    public void action_modificar(){
+        
+    }
+
+    public void action_eliminar(){
+        
+    }
+    
+    /**
+     * Creates a new instance of SeminarioVista
+     */
+    public SeminarioVista() {
+    }
+    
+}
